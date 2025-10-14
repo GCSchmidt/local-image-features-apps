f7b31cbabecbff917edac70345e195e8b35db90c
Gerhardt Schmidt <gerhardtschmidt2233@gmail.com>
2025-10-14 20:12:35 +0200

    implemented 1st stage of detecting and matching all keypoints in sticth_app.py

--- image-stitching/sticth_app.py
+++ image-stitching/sticth_app.py
@@ -5,6 +5,7 @@ import argparse
 from pathlib import Path
 import cv2
 import numpy as np
+import pandas as pd
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from utils import MatchResult
@@ -72,11 +73,127 @@ def match_features(img1_path: str, img2_path: str) -> MatchResult:
     return mr
 
 
+class StitchedImage():
+    """
+    A class that is used to construct the panorama result / final image.
+    """
+    def __init__(self, ref_image) -> None:
+        pass
 class Panorama():
 
     def __init__(self, path) -> None:
         self.folder_path = path
         self.image_files = get_image_files(path)
+        self.matching_df: pd.DataFrame
+        self.img_id_bounds: np.ndarray
+        self.K = 5  # find K-nearest neighbours during matching
+        self.M = 2  # number of best matched images to use per image
+
+    def generate_panorama(self):
+        kps = self.detect()
+        matches = self.match()
+
+
+    def detect(self):
+        """
+        Detects all keypoints in all the images and creates self.matching_df.
+
+        Returns:
+            tuple: tuple of OpenCV KeyPoint Objs
+        """
+        orb = cv2.ORB_create()
+        self.img_id_bounds = np.array([0], dtype=np.uint32)  # indicated at which ID an image starts/end
+        N = 0
+        des = np.empty((0, 32), dtype=np.uint8)  # data type is crucial!
+        img_ids = np.empty((0))
+        kps = ()
+
+        for img_id, img_path in enumerate(self.image_files): 
+            img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
+            kp_new, des_new = orb.detectAndCompute(img, None)  # type: ignore
+            des = np.vstack((des, des_new))
+            kps += kp_new
+            N_new = len(des_new)
+            N += N_new
+            self.img_id_bounds = np.append(self.img_id_bounds, N)
+            img_ids_new = np.full((N_new), img_id+1)
+            img_ids = np.concatenate((img_ids, img_ids_new), axis=0)
+
+        self.generate_matching_df(des, img_ids)
+
+        return kps
+
+    def generate_matching_df(self, des, img_ids) -> None:
+        self.matching_df = pd.DataFrame(
+            {"Descriptor": list(des),
+             "ImgId": img_ids})
+        # dont expect more than 2^8 / 255 images for the panorama
+        convert_dict = {'ImgId': np.uint8}
+        self.matching_df = self.matching_df.astype(convert_dict)
+
+    def match(self) -> None:
+        """
+        Adds 'Matched_Ids' collumns to self.matching_df, which contains ids 
+        of matched descriptors.
+        """
+        # Define FLANN parameters and match descriptors
+        # from https://docs.opencv.org/4.x/dc/dc3/tutorial_py_matcher.html
+        FLANN_INDEX_LSH = 6
+        index_params = dict(algorithm=FLANN_INDEX_LSH,
+                            table_number=6,  # 12
+                            key_size=12,     # 20
+                            multi_probe_level=1)  # 2
+        search_params = dict(checks=50)   # or pass empty dictionary
+        flann = cv2.FlannBasedMatcher(index_params, search_params)
+        des = np.stack(list(self.matching_df["Descriptor"]))
+        matches = flann.knnMatch(des, des, k=self.K)
+        # create Matched_Ids col. 1st match is always the same keypoint, therefore skip
+        self.matching_df['Matched_Ids'] = [np.array([m.trainIdx for m in Match[1:]]) for Match in matches]
+        self.add_matched_imgids_col()
+        # remove (invalid) matches from the same image
+        self.matching_df[["Matched_Ids", "Matched_ImgIds"]] = self.matching_df.apply(self.remove_invalid_matches, axis=1)
+        return matches
+
+    def add_matched_imgids_col(self) -> None:
+        """
+        Adds Matched_ImgIds column to self.matching_df.
+        """
+        self.matching_df['Matched_ImgIds'] = self.matching_df.apply(
+            self.matched_ids_to_img_ids,
+            axis=1
+            )
+        
+    def descriptor_id_to_img_id(self, id) -> int:
+        """
+        Get the associated Image ID for the given Descriptor ID, using img_id_bounds.
+        Returns:
+            int: in range from 1 to length of bounds
+        """
+        return np.searchsorted(self.img_id_bounds, id, side='right')
+
+    def matched_ids_to_img_ids(self, row):
+        matched_ids = row['Matched_Ids']
+        img_ids = [self.descriptor_id_to_img_id(id) for id in matched_ids]
+        return np.array(img_ids)
+
+    def remove_invalid_matches(self, row) -> pd.Series:
+        """
+        Remnoves Ids from Matched_Ids that are the are from the same Image (ImgId)
+
+        Args:
+            row: a row from a pandas dataframe
+
+        Returns:
+            series: a series for containing the updated Matched_Ids and Matched_ImgIds, with invalid values removed.
+        """
+        matched_ids, img_ids, imgId = row['Matched_Ids'], row['Matched_ImgIds'], row['ImgId'] 
+        indeces_to_remove = np.where(img_ids == imgId)
+        # create mask to remove values
+        mask = np.ones(len(matched_ids), dtype=bool)
+        mask[indeces_to_remove] = False
+        matched_ids = matched_ids[mask, ...]
+        img_ids = img_ids[mask, ...]
+        return pd.Series([matched_ids, img_ids])
 
     def match_stats(self):
         print(f"Match Stats of Images in {self.folder_path}")
@@ -95,8 +212,6 @@ class Panorama():
                     mr.n_inliers,
                     mr.percent_in))
 
-    def generate_panorama(self):
-        pass
 
 
 def main(args):
