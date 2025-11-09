bfe3617a777318024aa3c0d02cf9c5c8a49d39c7
Gerhardt Schmidt <gerhardtschmidt2233@gmail.com>
2025-11-09 21:49:22 +0100

    modified image-stitching pkg

---
+++ src/image-stitching/__init__.py
@@ -0,0 +1,2 @@
+# src/my_package/image-stitching/__init__.py
+# empty
---
+++ src/image-stitching/main.py
@@ -0,0 +1,32 @@
+import argparse
+import cv2
+import numpy as np
+from models import Panorama
+
+
+def main(args):
+    panorama = Panorama(args.folder_path)
+    panorama.generate_panorama()
+    panorama.match_stats()
+    return
+
+
+def parse_arguments():
+    """
+    Parses arguments
+    """
+    parser = argparse.ArgumentParser(
+                    prog='Stitchin App',
+                    description='This program stitches images in a folder together to create a panorama',
+                    epilog='Have fun!')
+
+    parser.add_argument('folder_path', help="path to folder containing images")  # positional argument
+    # parser.add_argument('--log', action='store_true', help="Enables logging to file")  # on/off flag
+    args = parser.parse_args()
+    return args
+
+
+if __name__ == "__main__":
+    print("Startring Sticthing App")
+    args = parse_arguments()
+    main(args)
