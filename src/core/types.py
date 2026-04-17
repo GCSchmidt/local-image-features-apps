d10862536b7a013460e4f7d30878b8c910e887f3
Gerhardt Schmidt <gerhardtschmidt2233@gmail.com>
2026-04-17 21:37:47 +0200

    added types.py for type alliases

---
+++ src/core/types.py
@@ -0,0 +1,20 @@
+# types.py
+
+import numpy as np
+import numpy.typing as npt
+import cv2
+from typing import TypeAlias
+
+# Macthes
+Match: TypeAlias = cv2.DMatch
+MatchGroup: TypeAlias = tuple[Match, ...]
+KnnMatches: TypeAlias = tuple[MatchGroup, ...]
+MatchList: TypeAlias = list[Match]
+KnnMatchesList: TypeAlias = list[MatchList]
+
+# Keypoints/Features
+KPGroup: TypeAlias = tuple[cv2.KeyPoint, ...]
+
+# Descriptors 
+ORBDescriptor: TypeAlias = npt.NDArray[np.uint8]   # (32,)
+ORBDescriptors: TypeAlias = npt.NDArray[np.uint8]  # (N, 32)
