2225e8daec7f9681610de07a273401caadf865bc
Gerhardt Schmidt <gerhardtschmidt2233@gmail.com>
2026-04-18 15:26:49 +0200

    added matches property to ORBImage

--- src/classes/ORBImage.py
+++ src/classes/ORBImage.py
@@ -1,11 +1,10 @@
 """
 
 """
-
 import cv2
-import numpy as np
+import os
 
-from core.types import ORBDescriptors, KPGroup
+from core.types import ORBDescriptors, KPGroup, KnnMatchesList
 
 
 class ORBImage():
@@ -16,6 +15,7 @@ class ORBImage():
         self._file_path = file_path
         self._kps: KPGroup | None = None
         self._descriptors: ORBDescriptors | None = None
+        self._matches: KnnMatchesList | None = None
 
     @property
     def kps(self):
@@ -24,8 +24,23 @@ class ORBImage():
     @property
     def descriptors(self):
         return self._descriptors
+    
+    @property
+    def matches(self):
+        return self._matches
 
-    def detect(self):
+    @property
+    def n_kps(self) -> int:
+        if self._kps is not None:
+            return len(self._kps)
+        else:
+            return 0
+
+    @property
+    def filename(self) -> str:
+        return os.path.basename(self._file_path)
+
+    def detect(self) -> None:
         img = cv2.imread(self._file_path, cv2.IMREAD_GRAYSCALE)
         if img is None:
             raise ValueError(f"Could not load image: {self._file_path}")
@@ -35,9 +50,8 @@ class ORBImage():
         if desc is not None:
             self._descriptors = desc
 
-    @property
-    def n_kps(self) -> int:
-        if self._kps is not None:
-            return len(self._kps)
+    def set_matches(self, matches: KnnMatchesList) -> None:
+        if len(matches) != self.n_kps:
+            raise Exception("Length of matches does not match length of self._kps")
         else:
-            return 0
+            self._matches = matches
