631f32b43d2163cc36aaf64d51f4863fae6bab9c
Gerhardt Schmidt <gerhardtschmidt2233@gmail.com>
2026-04-20 20:55:09 +0200

    added file paths to constants

--- src/core/constants.py
+++ src/core/constants.py
@@ -3,3 +3,7 @@
 # Panorama
 KNN_FOR_PANORAMA = 4
 M_CANDIDATE_IMAGES = 6
+
+# File Paths
+LOG_DIR = r'/home/schmidtg/coding_projetcs/local-image-features-apps/logs/'
+OUTPUT_DIR = r'/home/schmidtg/coding_projetcs/local-image-features-apps/output/'
