87a05ab16b4f74a4adad21fcd8e7634405bb5e46
Gerhardt Schmidt <gerhardtschmidt2233@gmail.com>
2026-05-01 19:01:04 +0200

    changed constants.py: - added MAX_IMAGE_DIM - modified INLIER_THRESHOLD

--- src/core/constants.py
+++ src/core/constants.py
@@ -1,9 +1,10 @@
-# constants
+# Image Processing
+MAX_IMAGE_DIM = 500  # Max width/height for feature detection
 
 # Panorama
 KNN_FOR_PANORAMA = 4  # number best macthes to find for each feature 
 M_CANDIDATE_IMAGES = 6  # number of images to check for connections
-INLIER_THRESHOLD = 0.25  # Percentage of inliers in the overlapping area required to confirm connection between images
+INLIER_THRESHOLD = 0.20  # Percentage of inliers in the overlapping area required to confirm connection between images
 GOOD_MATCH_RATIO = 0.75  # ratio needed for best match to be better than 2nd
 MIN_INLIERS = 30
 
