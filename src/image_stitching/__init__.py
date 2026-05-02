b671fdb6369a4421307e21f58c0e887a9e3faae4
Gerhardt Schmidt <gerhardtschmidt2233@gmail.com>
2026-05-02 16:08:34 +0200

    added CVPanorama to package

--- src/image_stitching/__init__.py
+++ src/image_stitching/__init__.py
@@ -1,6 +1,7 @@
 from image_stitching.pano_maker import PanoMaker
 from image_stitching.bundle_adjuster import BundleAdjuster, BundleResult
 from image_stitching.panograph import PanoGraph, ImageConnection
+from image_stitching.cv_panorama import CVPanorama
 from image_stitching.pipeline import render_panorama, match_images
 
 __all__ = [
@@ -9,6 +10,7 @@ __all__ = [
     "ImageConnection",
     "PanoGraph",
     "PanoMaker",
+    "CVPanorama",
     "match_images",
     "render_panorama",
 ]
